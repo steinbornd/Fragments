dd6fe6212d5296e155a1b4ffa8a826af133c830d
steinbornd <danielle.steinborn@hotmail.com>
2015-01-08 12:59:29 -0600

    Commit

---
+++ src/Ipsum.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.fragments;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Plato",
+        "Bertrand Russell",
+        "Aristotle",
+        "Descartes",
+        "Willard Van Orman Quine",
+        "Saul Kripke",
+        "Immanuel Kant",
+        "David Hume",
+        "George Berkeley"
+    };
+
+    static String[] Articles = {
+        "Plato's Works:\n\n-Apology\n-Charmides, or Temperance\n-Cratylus\n-Critias\n-Crito\n-Euthydemus\n-Euthyphro\n-Gorgias\n-Ion\n-Laches, or Courage\n-Laws\n-Lysis, or Friendship\n-Meno\n-Parmendes\n-Phaedo\n-Phaedrus\n-Philebus\n-Protagoras\n-The Republic",
+        "Bertrand Russell's Works:\n\n-German Social Democracy\n-An Essay on the Foundations of Geometry\n-The Principles of Mathematics\n-The Problems of Philosophy\n-Our Knowledge of the External World\n-Justice in War-Time\n-Why Men Fight\n-Political Ideals\n-Introduction to Mathematical Philosophy\n-Mysticism and Logic and Other Essays\n-The Problem of China\n-The Practice and Theory of Bolshevism\n-The Analysis of Mind\n-Free Thought and Official Propaganda\n-Icarus, or The Future of Science\n-The Conquest of Happiness\n-Prevent the Crime of Silence",
+        "Aristotle's Works:\n\n-The Athenian Constitution\n-Categories\n-On Dreams\n-On the Gait of Animals\n-On Generation and Corruption\n-On the Heavens\n-The History of Animals\n-On Interpretation\n-On Longevity and Shortness of Life\n-On Memory and Reminiscence\n-Metaphysics",
+        "Descartes' Works:\n\n-The Description of the Human\n-Dioptrique\n-Discourse on the Method\n-La Geometrie\n-Mediations on First Philosophy\n-Passions of the Soul\n-Principles of Philosophy\n-Rules for the Direction of the Mind\n-The World",
+        "Willard Van Orman Quine's Works:\n\n-A System of Logistic\n-Mathematical Logic\n-Selected Logic Papers\n-The Web of Belief\n-Elementary Logic\n-Methods of Logic\n-The Roots of Reference\n-The Ways of Paradox\n-Set Theory and Its Logic\n-The Time of My Life",
+        "Saul Kripke's Works:\n\n-Naming and Necessity\n-Wittgenstein on Rules and Private Language: an Elementary Exposition\n-Philosophical Troubles\n-Reference and Existence, The John Locke Lectures\n-A Completeness Theorem in Modal Logic\n-Distinguished Constituents",
+        "Immanuel Kant's Works:\n\n-Thoughts on the True Estimation of Vital Forces\n-A New Elucidation of the First Principles of Metaphysical Cognition\n-Universal Natural History and Theory of Heaven\n-Monadologia Physica\n-The False Subtlety of the Four Syllogistic Figures\n-The Only Possible Argument in Support of a Demonstration of the Existence of God",
+        "David Hume's Works:\n\n-A Kind of History of My Life\n-A Treatise of Human Nature\n-An Abstract of a Book leately Published: Entitled A Treatise of Human Nature\n-Essays Moral and Political\n-A Letter from a Gentleman to His Friend in Edinburgh\n-An Enquiry Concerning Human Understanding\n-Political Discourses",
+        "George Berkeley's Works:\n\n-Arithmetica\n-Miscellanea Mathematica\n-Philosophical Commentaries\n-An Essay towards a New Theory of Vision\n-Passive Obedience\n-Three Dialogues between Hylas and Philonous\n-An Essage Towards Preventing the Ruin of Great Britain\n-De Motu\n-The Querist"
+    };
+}
